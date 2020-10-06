ab4f08b8080c0ad2a8c0bbf555e1ae9913a94ac8
Clement Gaston <clem31130@hotmail.fr>
2020-10-06 21:32:59 +0200

    Adding contact + CSS

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -6,8 +6,10 @@ function Contact() {
   return (
     <Section id={"contact"} title={"Contact"}>
         <form>
-            <input type="text" placeholder="Nom"></input>
-            <input type="email" placeholder="Adresse email"></input>
+            <div className="boxContainer">
+              <input type="text" placeholder="Nom"></input>
+              <input type="email" placeholder="Adresse email"></input>
+            </div>
             <input type="text" placeholder="Sujet"></input>
             <input type="textarea" placeholder="Message"></input>
             <button type="submit">Envoyer</button>
